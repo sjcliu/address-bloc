d19e95c3877f08dd80b914211b1c239955748307
Stanly Liu <sjcliu@gmail.com>
2016-01-25 23:58:16 +1100

    Assignment 19 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -7,14 +7,26 @@ class AddressBook
     @entries = []
   end
 
+  def remove_entry(name, phone_number, email)
+    delete_entry = nil #Why does it = nil
+
+    @entries.each do |entry|
+      if name == entry.name && phone_number == entry.phone_number && email == entry.email
+        delete_entry = entry # i'm getting confused with entry and entries
+      end
+    end
+
+    @entries.delete(delete_entry) #What does this do?
+  end
+
   def add_entry(name, phone_number, email)
-    index = 0
+    index = 0 # What does this do?
     @entries.each do |entry|
       if name < entry.name
         break
       end
       index += 1
    end
-   @entries.insert(index, Entry.new(name, phone_number,email))
+   @entries.insert(index, Entry.new(name, phone_number,email))# Need this explained
   end
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -36,4 +36,42 @@ RSpec.describe AddressBook do
       expect(new_entry.email).to eq ('augusta.king@lovelace.com')
     end
   end
+  ## This is what i did
+  # describe "#remove_entry" do
+  #   it "should remove one entry from the address book" do
+  #     book = AddressBook.new
+  #     book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+  #
+  #     expect(book.entries.size).to eq(0)
+  #   end
+  #
+  #   it "confirms entry has been deleted" do
+  #     book = AddressBook.new
+  #     book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+  #     new_entry = book.entries[0]
+  #
+  #     expect(remove_entry.name).to eq()
+  #     expect(remove_entry.phone_number).to eq()
+  #     expect(remove_entry.email).to eq()
+  #   end
+  # end
+  describe "#remove_entry" do
+    it "should remove on entry from the address book" do
+      book = AddressBook.new
+      # I dont understand the logic here why will we add an entry
+      book.add_entry("name", "phone_number","email")
+
+      #Why do we do the code below?
+      name = "Ada Lovelace"
+      phone_number = "010.012.1815"
+      email = "augusta.king@lovelace.com"
+      book.add_entry(name, phone_number, email)
+
+      #what does the .size and eq do, Where did "entries" come from?
+      expect(book.entries.size).to eq 2
+      book.remove_entry(name, phone_number, email)
+      expect(book.entries.size).to eq 1
+      expect(book.entries.first.name).to eq("name")
+    end
+  end
 end
